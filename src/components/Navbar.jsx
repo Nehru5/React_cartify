c7f2f2f21fed3b1793e3e62ea4808aec1d4ac556
Nehru raja <nehruraja9485@gmail.com>
2026-04-27 09:18:51 +0530

    Day 1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <div className="left">
+          <h2>Cartify</h2>
+        </div>
+
+        <div className="right">
+          <h2>Home</h2>
+          <h2>Products</h2>
+          <h2>Orders</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const Homepage = () => {
+  return (
+    <>
+    <Navbar/>
+      <div className="hero-section">
+        <h1>Welcome to <span style={{color:"orange"}}>Cartify</span>🛒</h1>
+        <h2>Discover the best products at unbeatable prices</h2>
+        <button>Shop now</button>
+      </div>
+
+      <center><h1>Shop by Category</h1></center>
+
+      <div className="products">
+        <img src="https://5.imimg.com/data5/SELLER/Default/2022/1/QZ/AO/RT/142262681/istockphoto-1301394040-170667a.jpg" alt="" />
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/068/733/167/small/the-apple-products-that-are-on-the-table-uniquely-crafted-for-your-design-needs-photo.jpg" alt="" />
+        <img src="https://dalpl.co.in/wp-content/uploads/2024/05/bigstock-Wicker-Basket-With-Assorted-Or-56073449.jpg" alt="" />
+      </div>
+
+      
+      <div className="products">
+        <img src="https://5.imimg.com/data5/SELLER/Default/2022/1/QZ/AO/RT/142262681/istockphoto-1301394040-170667a.jpg" alt="" />
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/068/733/167/small/the-apple-products-that-are-on-the-table-uniquely-crafted-for-your-design-needs-photo.jpg" alt="" />
+        <img src="https://dalpl.co.in/wp-content/uploads/2024/05/bigstock-Wicker-Basket-With-Assorted-Or-56073449.jpg" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default Homepage
